25c53eab7f2651fa037a66bcf98b147b64045deb
diablo <progzone122@gmail.com>
2024-10-04 11:13:25 +0000

    .

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,12 @@ pub mod parser;
 pub mod models;
 pub mod schema;
 
-use std::fs::File;
-use std::{env, io};
+use std::env;
 use diesel::{Connection, MysqlConnection, QueryResult};
 use dotenvy::dotenv;
-use serde_json::to_writer;
-use parser::news::News;
 use parser::errors::Error;
 use parser::post::Post;
 use crate::models::news::NewNews;
-use crate::models::posts;
 use crate::models::posts::{NewPost, Posts};
 use crate::parser::errors::Errors;
 use crate::parser::news;
@@ -95,52 +91,4 @@ async fn main() {
             }
         };
     }
-
-    //
-    // // Create news and posts vectors
-    // let mut news_vec: Vec<news> = Vec::new();
-    // let mut posts: Vec<Post> = Vec::new();
-    //
-    // for page in 1..=PAGES {
-    //     match news::new(ENDPOINT, page).await {
-    //         Ok(news) => {
-    //             news_vec.push(news.clone()); // Store news for writing later
-    //
-    //             // For each post in the news, create a Post instance using its URL
-    //             for post in news.posts {
-    //                 if let Some(url) = post.url {
-    //                     match Post::new(url.clone()).await {
-    //                         Ok(post_detail) => {
-    //                             posts.push(post_detail);
-    //                         }
-    //                         Err(e) => {
-    //                             eprintln!("Error fetching post detail for URL {}: {:#?}", url, e);
-    //                         }
-    //                     }
-    //                 } else {
-    //                     eprintln!("Post has no URL, skipping.");
-    //                 }
-    //             }
-    //         }
-    //         Err(e) => {
-    //             eprintln!("Error fetching news from page {}: {:#?}", page, e);
-    //         }
-    //     };
-    // }
-    //
-    // // Write all news to news.json
-    // let mut news_file: File = File::create("./result/news.json")?;
-    // if let Err(e) = to_writer(&mut news_file, &news_vec) {
-    //     eprintln!("Error writing news to JSON: {}", e);
-    // }
-    //
-    // // Write all posts to posts.json
-    // let mut posts_file: File = File::create("./result/posts.json")?;
-    // if let Err(e) = to_writer(&mut posts_file, &posts) {
-    //     eprintln!("Error writing posts to JSON: {}", e);
-    // }
-
-    // Ok(())
-
-
 }
